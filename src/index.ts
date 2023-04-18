01f455fadefc29ea32ed88374eeddfab4a30ab89
hlm <hlm@mit.com.mm>
2023-04-18 11:34:38 +0630

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // src/index.ts
 export function isValidMyanmarNRC(nrc: string): boolean {
-  const regex =
-    /^([1-9][0-9]{0,2})\/(([A-Za-z]|[0-9][0-9])?[A-Za-z]{1,2})\/([0-9]{6})$/;
+  const regex = /^([1-9][0-9]{0,2})\/([A-Za-z]{1,6})\([A-Za-z]\)([0-9]{6})$/;
   return regex.test(nrc);
 }
